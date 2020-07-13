e5456513822fe6bd79d6bb789ee8c4c75ac60fd2
Taylor Somers <tsomers@yandex.com>
2020-07-13 11:56:50 -0400

    Add constructor for car objects

--- Car.cs
+++ Car.cs
@@ -7,6 +7,13 @@ public class Car
   public int Price;
   public int Miles;
 
+  public Car(string makeModel, int price, int miles)
+  {
+    MakeModel = makeModel;
+    Price = price;
+    Miles = miles;
+  }
+
   public bool WorthBuying(int maxPrice)
   {
     return (Price < maxPrice);
