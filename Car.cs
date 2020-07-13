b46f4cf31feefc07d9a3962337ce2e7f84ba3977
Taylor Somers <tsomers@yandex.com>
2020-07-13 11:38:50 -0400

    Add Car objects

--- Car.cs
+++ Car.cs
@@ -12,6 +12,24 @@ public class Program
 {
   public static void Main()
   {
-    
+    Car volkswagen = new Car();
+    volkswagen.MakeModel = "1974 Volkswagen Thing";
+    volkswagen.Price = 1100;
+    volkswagen.Miles = 368792;
+
+    Car yugo = new Car();
+    yugo.MakeModel = "1980 Yugo Koral";
+    yugo.Price = 700;
+    yugo.Miles = 56000;
+
+    Car ford = new Car();
+    ford.MakeModel = "1988 Ford Country Squire";
+    ford.Price = 1400;
+    ford.Miles = 239001;
+
+    Car amc = new Car();
+    amc.MakeModel = "1976 AMC Pacer";
+    amc.Price = 400;
+    amc.Miles = 198000;
   }
 }
